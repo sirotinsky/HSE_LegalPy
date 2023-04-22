9ae83e26bc6dd7987d497a3af966b94b812dcf4e
kirill <kirill@sirotinsky.com>
2023-04-22 13:05:13 +0300

    Второй урок

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -197,8 +197,29 @@ def is_org_inn(inn: str):
         return False
 
 
+my_inn = "263411857320"
+
+
 def is_person_inn(inn: str):
-    return
+    k_1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    k_2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    sum_1 = 0
+    sum_2 = 0
+    for i, c in enumerate(inn):
+        if i > 9:
+            break
+        sum_1 += (int(c) * k_1[i])
+    sum_1 = sum_1 % 11
+    if sum_1 > 9:
+        sum_1 = sum_1 % 10
+    for i, c in enumerate(inn):
+        if i > 10:
+            break
+        sum_2 += (int(c) * k_2[i])
+    sum_2 = sum_2 % 11
+    if sum_2 > 9:
+        sum_2 = sum_2 % 10
+    pass
 
 
 def is_valid_inn(inn: str):
@@ -211,14 +232,14 @@ def is_valid_inn(inn: str):
 
 
 def task_3():
-    for i in respondents:
-        print(is_valid_inn(i["inn"]))
+
+    print(is_valid_inn(my_inn))
 
 
 def main():
-    task_1()
+    # task_1()
     # task_2()
-    # task_3()
+    task_3()
 
 
 if __name__ == "__main__":
